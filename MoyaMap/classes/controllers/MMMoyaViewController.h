97d89ae11da8296618de2ff7b901f8b3a5eeef8a
Hal Seki <hal@plants-web.jp>
2013-02-16 17:10:07 +0900

    added MoyaViewController

---
+++ MoyaMap/classes/controllers/MMMoyaViewController.h
@@ -0,0 +1,14 @@
+//
+//  MMMoyaViewController.h
+//  MoyaMap
+//
+//  Created by Haruyuki Seki on 2/16/13.
+//  Copyright (c) 2013 Hacker's Cafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMMoyaViewController : UITableViewController
+- (IBAction)pressClose:(id)sender;
+
+@end
